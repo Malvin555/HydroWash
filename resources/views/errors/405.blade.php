0e0bc6cd85e09774c803e81061e4c563bd33730c
Malvin555 <malvinbrine555@gmail.com>
2025-04-29 21:24:10 +0800

    feat: update all packages, and add two additional error pages[debug=false]

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.error-layout')
+@section('code', '405')
+@section('message', 'Method Not Allowed')
+@section('description', 'The method specified in the request is not allowed for the resource identified by the request URI. Please check the request method and try again.')
