b1f1b6e7a6753a0e0cebad5590588fb8e869598d
Malvin555 <malout056@outlook.com>
2025-09-30 19:32:13 +0800

    Add Vercel deployment configuration and HTTPS enforcement
    Add .vercelignore to exclude vendor directory Add .vercel configuration
    file Add API entry point for Vercel routing Enforce HTTPS in production
    environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,16 +7,19 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
-    }
+     public function register()
+            {
+                if (env('APP_ENV') !== 'local') {
+                    URL::forceScheme('https');
+                }
+            }
 
     /**
      * Bootstrap any application services.
